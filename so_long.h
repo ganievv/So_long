49341aca36ed4b42f7e4987f309dfc59543c4870
Semen Ganiev <sganiev@3-B-2.42heilbronn.de>
2024-05-06 18:24:24 +0200

    Created .h file

---
+++ so_long.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 17:13:30 by sganiev           #+#    #+#             */
+/*   Updated: 2024/05/06 18:17:00 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "mlx/mlx.h"
+
+// for get_next_line
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(const char *s1);
+char	*get_next_line(int fd);
+
+#endif
