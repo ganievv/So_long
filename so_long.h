114ebca03645251847d3033159241803d5821bcf
Semen Ganiev <sganiev@3-B-8.42heilbronn.de>
2024-05-09 13:15:09 +0200

    Created .h file with lib and data types for a map

--- so_long.h
+++ so_long.h
@@ -6,28 +6,29 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:13:30 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/06 18:17:00 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/09 12:57:37 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
 # include "mlx/mlx.h"
+# include "libft/libft.h"
+# include <stdio.h>
 
-// for get_next_line
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 4
-# endif
+typedef struct s_list
+{
+	char			*line;
+	struct s_list	*next;
+}	t_list;
 
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen(const char *s);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(const char *s1);
-char	*get_next_line(int fd);
+typedef struct map_data {
+	t_list	*map;
+	int		width;
+	int		hight;
+}	t_map_data;
+
+void	check_width_hight(t_map_data *m_data);
 
 #endif
