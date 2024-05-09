25d7ceb8a1c8c91322e98e2e66adb2f4c7462644
Semen Ganiev <sganiev@3-B-8.42heilbronn.de>
2024-05-09 15:20:38 +0200

    Created a function to check validity of a map

--- so_long.c
+++ so_long.c
@@ -6,11 +6,25 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:15:59 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/09 13:10:51 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/09 14:09:31 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+// you should use your own ft_printf !!!
+
+// you should check comments in test_1.c file
+
+// you don't allocate memory for m_data->map, but only for strings
+// maybe you should use strjoin but you own version? or maybe 
+// you should usse just 1 string and strjoin? or fixed size buffer (e.g. 1000) ?
+
+void	check_map(t_map_data *m_data)
+{
+	check_isempty(m_data->map);
+	check_width_hight(m_data);
+}
 
 void	append_node(t_list **head, char *line)
 {
@@ -20,7 +34,7 @@ void	append_node(t_list **head, char *line)
 	new_node = (t_list *)malloc(sizeof(t_list));
 	if (!new_node)
 	{
-		perror("Failed to allocate node");
+		perror("Error\n!!!Failed to allocate node!!!\n");
 		exit(1);
 	}
 	new_node->line = line;
@@ -31,9 +45,7 @@ void	append_node(t_list **head, char *line)
 	{
 		current = *head;
 		while (current->next != NULL)
-		{
 			current = current->next;
-		}
 		current->next = new_node;
 	}
 }
@@ -63,7 +75,7 @@ void	open_map(char *map)
 	fd = open(map, O_RDONLY);
 	if (fd < 0)
 	{
-		perror("Failed to open file");
+		perror("Error\n!!!Failed to open file!!!\n");
 		exit(1);
 	}
 	read_map(fd, &m_data);
@@ -74,7 +86,7 @@ int	main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		ft_printf("Error!!! You need to pass .ber file and nothing else\n");
+		ft_printf("Error\n!!!You need to pass .ber file and nothing else!!!\n");
 		return (1);
 	}
 	else
