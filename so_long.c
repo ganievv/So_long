61adea0808a061e62085c79de4c2487e6fa7a41d
Semen Ganiev <sganiev@3-B-8.42heilbronn.de>
2024-05-09 13:10:30 +0200

    Created a map reading function

--- so_long.c
+++ so_long.c
@@ -6,11 +6,28 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:15:59 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/09 13:06:32 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/09 13:09:20 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	read_map(int fd, t_map_data *m_data)
+{
+	char	*line;
+	t_list	*head;
+
+	head = NULL;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		append_node(&head, line);
+	}
+	m_data->map = head;
+	check_map(m_data);
+}
 
 void	open_map(char *map)
 {
