0d8d2b31002225d074ed08eed9b10ec062daf1cd
Semen Ganiev <sganiev@3-C-10.42heilbronn.de>
2024-05-11 20:12:39 +0200

    Checked a map for the presence of .ber extension

---
+++ check_file_name.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file_name.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 19:36:49 by sganiev           #+#    #+#             */
+/*   Updated: 2024/05/11 19:50:06 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int	check_file_name(char *name)
+{
+	int	line_length;
+
+	line_length = ft_strlen(name);
+	if (line_length > 4 && (ft_strncmp(name + line_length - 4, ".ber", 4) == 0))
+		return (0);
+	else
+		return (1);
+}
