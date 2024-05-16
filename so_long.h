14f658bd9c3e39013ab2a0f8a652498a9c89d92e
Semen Ganiev <sganiev@3-G-11.42heilbronn.de>
2024-05-16 17:05:11 +0200

    Added prototypes of needed functions

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:13:30 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/09 12:57:37 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/16 15:24:59 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,32 @@ typedef struct s_list
 
 typedef struct map_data {
 	t_list	*map;
+	char	**map_arr;
+	char	**map_c;
 	int		width;
 	int		hight;
+	int		collectible_total;
+	int		collectible_current;
 }	t_map_data;
 
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+void	check_isempty(t_list *map);
+void	check_wall(t_list *map);
 void	check_width_hight(t_map_data *m_data);
+void	free_list(t_list *head);
+void	check_player(t_list *map);
+void	check_exit(t_list *map);
+void	check_for_valid_symbols(t_list *map);
+int		check_file_name(char *name);
+void	check_collectible(t_map_data *m_data);
+void	check_path(t_map_data *m_data);
+void	flood_fill(char **tab, t_point size, t_point begin);
+void	free_arr(char **arr, int hight);
+void	item_search(t_map_data *m_data, t_point *player, char item);
 
 #endif
