47c0fc06fb125e71dd1b42772fe347fe05830326
Semen Ganiev <sganiev@3-G-11.42heilbronn.de>
2024-05-16 15:18:05 +0200

    Moved flood_fill from the check_path file

---
+++ flood_fill.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 15:14:16 by sganiev           #+#    #+#             */
+/*   Updated: 2024/05/16 15:15:40 by sganiev          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	flood_fill(char **tab, t_point size, t_point begin)
+{
+	t_point	up;
+	t_point	down;
+	t_point	left;
+	t_point	right;
+
+	if (begin.x < 0 || begin.x >= size.x || begin.y < 0
+		|| begin.y >= size.y || tab[begin.y][begin.x] != '0')
+		return ;
+	tab[begin.y][begin.x] = 'F';
+	up.x = begin.x;
+	up.y = begin.y - 1;
+	down.x = begin.x;
+	down.y = begin.y + 1;
+	left.x = begin.x - 1;
+	left.y = begin.y;
+	right.x = begin.x + 1;
+	right.y = begin.y;
+	flood_fill(tab, size, up);
+	flood_fill(tab, size, down);
+	flood_fill(tab, size, left);
+	flood_fill(tab, size, right);
+}
