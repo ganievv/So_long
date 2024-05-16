fe24c70c932bb766e8336f149ea4726f64da19bd
Semen Ganiev <sganiev@3-G-11.42heilbronn.de>
2024-05-16 15:29:14 +0200

    Moved from this file some functions to new files

--- check_path.c
+++ check_path.c
@@ -6,42 +6,11 @@
 /*   By: sganiev <sganiev@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 13:19:53 by sganiev           #+#    #+#             */
-/*   Updated: 2024/05/15 19:50:36 by sganiev          ###   ########.fr       */
+/*   Updated: 2024/05/16 15:24:29 by sganiev          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-typedef struct s_point
-{
-	int	x;
-	int	y;
-}	t_point;
-
-static void	flood_fill(char **tab, t_point size, t_point begin)
-{
-	t_point	up;
-	t_point	down;
-	t_point	left;
-	t_point	right;
-
-	if (begin.x < 0 || begin.x >= size.x || begin.y < 0
-		|| begin.y >= size.y || tab[begin.y][begin.x] != '0')
-		return ;
-	tab[begin.y][begin.x] = 'F';
-	up.x = begin.x;
-	up.y = begin.y - 1;
-	down.x = begin.x;
-	down.y = begin.y + 1;
-	left.x = begin.x - 1;
-	left.y = begin.y;
-	right.x = begin.x + 1;
-	right.y = begin.y;
-	flood_fill(tab, size, up);
-	flood_fill(tab, size, down);
-	flood_fill(tab, size, left);
-	flood_fill(tab, size, right);
-}
 
 static char	**to_array(t_map_data *m_data)
 {
@@ -62,75 +31,95 @@ static char	**to_array(t_map_data *m_data)
 	return (arr);
 }
 
-void	player_search(t_map_data *m_data, char **arr, t_point *player)
+static void	symb_search(t_map_data *m_data,
+	t_point *player, t_point *begin, char symb)
 {
-	player->x = 0;
-	player->y = 0;
-	while (player->y < m_data->hight)
-	{
-		while (player->x < m_data->width)
-		{
-			if (arr[player->y][player->x] == 'P')
-				return ;
-			player->x++;
-		}
-		player->x = 0;
-		player->y++;
-	}
-}
-
-void	begin_search(char **arr, t_point *size,
-	t_point *player, t_point *begin)
-{
-	if (player->y - 1 > 0 && arr[player->y - 1][player->x] == '0')
+	if (player->y - 1 > 0 && m_data->map_arr[player->y - 1][player->x] == symb)
 	{
 		begin->x = player->x;
 		begin->y = player->y - 1;
 	}
-	if (player->y + 1 < size->y - 1 && arr[player->y + 1][player->x] == '0')
+	else if (player->y + 1 < m_data->hight - 1
+		&& m_data->map_arr[player->y + 1][player->x] == symb)
 	{
 		begin->x = player->x;
 		begin->y = player->y + 1;
 	}
-	if (player->x - 1 > 0 && arr[player->y][player->x - 1] == '0')
+	else if (player->x - 1 > 0
+		&& m_data->map_arr[player->y][player->x - 1] == symb)
 	{
 		begin->x = player->x - 1;
 		begin->y = player->y;
 	}
-	if (player->x + 1 < size->x - 1 && arr[player->y][player->x + 1] == '0')
+	else if (player->x + 1 < m_data->width - 1
+		&& m_data->map_arr[player->y][player->x + 1] == symb)
 	{
 		begin->x = player->x + 1;
 		begin->y = player->y;
 	}
 }
 
+static void	isinvalid(t_point *begin, t_map_data *m_data)
+{
+	if (begin->x == 0 && begin->y == 0)
+	{
+		ft_printf("Error\n!!!The player can't reach every item!!!\n");
+		free_list(m_data->map);
+		free_arr(m_data->map_arr, m_data->hight);
+		exit(1);
+	}
+}
+
+static void	check_flood(t_map_data *m_data)
+{
+	t_point	item;
+	t_point	begin;
+
+	item.x = 0;
+	item.y = 0;
+	begin.x = 0;
+	begin.y = 0;
+	while (item.y < m_data->hight)
+	{
+		while (item.x < m_data->width)
+		{
+			if (m_data->map_arr[item.y][item.x] == 'C'
+				|| m_data->map_arr[item.y][item.x] == 'E')
+			{
+				symb_search(m_data, &item, &begin, 'F');
+				isinvalid(&begin, m_data);
+			}
+			item.x++;
+		}
+		item.x = 0;
+		item.y++;
+	}
+}
+
 void	check_path(t_map_data *m_data)
 {
-	char	**arr_copy;
 	t_point	size;
 	t_point	player;
 	t_point	begin;
 
 	m_data->map_arr = to_array(m_data);
-	arr_copy = to_array(m_data);
-	if (m_data->map_arr == NULL || arr_copy)
+	m_data->map_c = to_array(m_data);
+	if (m_data->map_arr == NULL || m_data->map_c == NULL)
 	{
+		ft_printf("Error\n!!!Failed to accocate memory "
+			"for a copy of the map!!!\n");
+		free_list(m_data->map);
+		free_arr(m_data->map_arr, m_data->hight);
+		free_arr(m_data->map_c, m_data->hight);
+		exit(1);
 	}
 	size.x = m_data->width;
 	size.y = m_data->hight;
-	player_search(m_data, arr_copy, &player);
-	begin_search(arr_copy, &size, &player, &begin);
-	flood_fill(arr_copy, size, begin);
+	begin.x = 0;
+	begin.y = 0;
+	item_search(m_data, &player, 'P');
+	symb_search(m_data, &player, &begin, '0');
+	isinvalid(&begin, m_data);
+	flood_fill(m_data->map_arr, size, begin);
+	check_flood(m_data);
 }
-
-//void	fill(char **tab, t_point size, t_point begin)
-//{
-//	if (begin.y + 1 < size.y && tab[begin.x][begin.y + 1] == '0')
-//		tab[begin.x][begin.y + 1] = 'F';
-//	if (begin.x + 1 < size.x && tab[begin.x + 1][begin.y] == '0')
-//		tab[begin.x + 1][begin.y] = 'F';
-//	if (begin.y - 1 > 1 && tab[begin.x][begin.y - 1] == '0')
-//		tab[begin.x][begin.y - 1] = 'F';
-//	if (begin.x - 1 > 1 && tab[begin.x - 1][begin.y] == '0')
-//		tab[begin.x - 1][begin.y] = 'F';
-//}
